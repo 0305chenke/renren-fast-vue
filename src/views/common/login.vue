71e0725ad214d7d473111bdcc31543564d51028d
0305chenke <ckke0305@sina.com>
2020-01-16 17:36:31 +0800

    增加商品管理

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,8 +3,8 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <h2 class="brand-info__text">My-First-Web</h2>
+          <p class="brand-info__intro">这是我的第一个网站，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
@@ -112,11 +112,10 @@
       position: fixed;
       top: 0;
       left: 0;
-      z-index: -1;
       width: 100%;
       height: 100%;
       content: "";
-      background-image: url(~@/assets/img/login_bg.jpg);
+      background-image: url(~@/assets/img/login.jpg);
       background-size: cover;
     }
     .site-content__wrapper {
@@ -153,12 +152,12 @@
     }
     .login-main {
       position: absolute;
-      top: 0;
+      top: 150px;
       right: 0;
-      padding: 150px 60px 180px;
+      padding: 10px 40px 30px;
       width: 470px;
-      min-height: 100%;
-      background-color: #fff;
+      min-height: 30%;
+      background:rgba(54, 200, 151, 0);
     }
     .login-title {
       font-size: 16px;
